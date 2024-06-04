f8c9ac9aed8191d7fc7bda496ea5c7e88adc71fe
Iceline King <icedking23@outlook.com>
2024-06-04 10:02:34 -0400

    got exercise 2 working

--- exercise1/scripts/users.js
+++ exercise1/scripts/users.js
@@ -1,9 +1,9 @@
 "use strict";
 
-console.log("title");
+console.log("address");
 
-// https://jsonplaceholder.typicode.com/todos/1
-const apiBaseUrl = "https://jsonplaceholder.typicode.com/todos/";
+// https://jsonplaceholder.typicode.com/users
+const apiBaseUrl = "https://jsonplaceholder.typicode.com/users/";
 
 window.onload = function () {
   const getResultsButton = document.getElementById("getResultsButton");
@@ -25,21 +25,15 @@ function onGetResultsButtonClick() {
     .then((data) => {
       console.log(data);
 
-      resultsOutPut.innerHTML = data.title;
+      // "for in :" to retrive object keys --ex: name/address/number \ "for of :" to loop through an array
+      for (let d in data) {
+        let table = document.createElement("table");
+        table.innerHTML = data[d];
 
+        resultsOutPut.appendChild(table);
+
+        // calling keys (name of variable) + the actual variable; the bracket calls values of the keys
+        console.log(d + ": " + data[d]);
+      }
     });
 }
-
-
-
-
-
-
-
-
-  //   for (let title of data.title) {
-      //     let div = document.createElement("div");
-      //     div.innerHTML = title;
-
-      //     resultsOutPut.appendChild(div);
-      //   }
