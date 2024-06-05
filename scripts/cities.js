2146b19f1281bbe13254c204916a37ecc04e885b
Iceline King <icedking23@outlook.com>
2024-06-05 14:11:13 -0400

    added sample cities array

---
+++ scripts/cities.js
@@ -0,0 +1,59 @@
+
+let cities = [
+
+
+    { name: "Benbrook, TX",
+    latitude: 32.6732,
+    longitude: -97.4606 },
+
+    { name: "Phoenix, AZ",
+    latitude: 33.375919197988615,
+    longitude: -112.30501142093024 },
+
+    { name: "Santa Clara, CA",
+    latitude: 34.3923,
+    longitude: -118.5433 },
+
+    { name: "Minneapolis, MN",
+    latitude: 44.9764,
+    longitude: -93.2474 },
+
+    { name: "Las Vegas, NV",
+    latitude: 36.1714,
+    longitude: -115.1544 },
+    
+]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   ];
